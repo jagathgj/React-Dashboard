75079cd31b4f8b29ca61cee9a7a53a59350d2517
Jagath Jayakumar <Jagath.J@india-powai.orioninc.com>
2020-08-23 23:20:44 +0530

    dropdown style Ui issues fixed

--- src/domains/AdminPlaces/AdminPlaces.js
+++ src/domains/AdminPlaces/AdminPlaces.js
@@ -7,7 +7,7 @@ const AdminPlaces = () => {
       <div className="admin-places-section flex-none m-3 p-3 bg-white rounded shadow-sm d-flex flex-column">
         <FilterBar showDate={false}></FilterBar>
       </div>
-      <div className="place-list-section flex-fill mt-2 h-100">
+      <div className="place-list-section flex-fill h-100 m-3 bg-white rounded shadow-sm">
         <HeaderTable></HeaderTable>
       </div>
     </>
